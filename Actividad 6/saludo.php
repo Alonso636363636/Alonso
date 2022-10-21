415dd3ce7ab556535c9bdf0de03af740768c89c6
Alonso636363636 <fernando20435@cbtis75.edu.mx>
2022-10-21 09:06:56 -0500

    Create saludo.php

---
+++ Actividad 6/saludo.php
@@ -0,0 +1,33 @@
+<!-- 
+    Descripcion: Saludo
+    Author:Fernando Alonso Fernandez Martinez
+    Fecha: 21/10/2022
+ -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <form action="Saludito.php" method="POST">
+    <h1>Ingresa tu nombre Crack =></h1>
+    <input type="text" name="nombre">
+    <input type="submit" value="Saludar"/>
+    </form>
+    </center>
+
+    <center>
+        <h2>
+            <br><br>
+            Hecho por: Fernando Alonso Fernandez Martinez 
+            <br>
+            Fecha:21/10/2022
+        </h2>
+    </center>
+</body>
+</html>
