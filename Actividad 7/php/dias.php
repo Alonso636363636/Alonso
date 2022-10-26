1482089b4089bad7189915454e96c3ec38fa49a6
Alonso636363636 <80420434+Alonso636363636@users.noreply.github.com>
2022-10-26 11:57:10 -0500

    Create dias.php

---
+++ Actividad 7/php/dias.php
@@ -0,0 +1,36 @@
+<!-- 
+    Descripcion: Dias vividos
+    Autor: Fernando Alonso Fernandez Martinez
+    Fecha: 24/10/2022
+ -->
+ <!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Dias vividos</title>
+</head>
+
+<body background="../img/piso-madera-blanca-textura-fondo_24076-493.webp">
+
+    <form action="greeting.php" method="get">
+        <center>
+            <div class="titulo">
+                <h1>Dias vividos</h1>
+            </div>
+            <hr>
+            <div class="Formulario">
+                    <input type="number" name="edad" id="edad" placeholder="Ingresa tu edad "><br><br>
+                    <button type="submit">Calcular dias vividos</button>
+                
+            </div>
+        </center>
+
+    </form>
+    </div>
+</body>
+
+</html>
